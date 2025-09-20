875a9ea3fa90b47666bc96450f9ee1d9e18e3782
1ureka <summerheadquarters001@gmail.com>
2025-09-20 17:39:02 +0800

    重構 electron 物件，移除 ipcRenderer 封裝，簡化 API 接口

--- src/ipc.d.ts
+++ src/ipc.d.ts
@@ -3,12 +3,10 @@ export {};
 declare global {
   interface Window {
     electron: {
-      ipcRenderer: {
-        send: (channel: string, ...args: any[]) => void;
-        on: (channel: string, listener: (...args: any[]) => void) => void;
-        off: (channel: string, listener?: (...args: any[]) => void) => void;
-        request: (channel: string, ...args: any[]) => Promise<any>;
-      };
+      send: (channel: string, ...args: any[]) => void;
+      on: (channel: string, listener: (...args: any[]) => void) => void;
+      off: (channel: string, listener?: (...args: any[]) => void) => void;
+      request: (channel: string, ...args: any[]) => Promise<any>;
     };
   }
 }
--- src/preload.ts
+++ src/preload.ts
@@ -1,22 +1,20 @@
 import { contextBridge, ipcRenderer } from "electron";
 
 contextBridge.exposeInMainWorld("electron", {
-  ipcRenderer: {
-    send: (channel: string, ...args: any[]) => {
-      ipcRenderer.send(channel, ...args);
-    },
-    on: (channel: string, listener: (...args: any[]) => void) => {
-      ipcRenderer.on(channel, (_, ...args) => listener(...args));
-    },
-    off: (channel: string, listener?: (...args: any[]) => void) => {
-      if (listener) {
-        ipcRenderer.off(channel, listener);
-      } else {
-        ipcRenderer.removeAllListeners(channel);
-      }
-    },
-    request: (channel: string, ...args: any[]): Promise<any> => {
-      return ipcRenderer.invoke(channel, ...args);
-    },
+  send: (channel: string, ...args: any[]) => {
+    ipcRenderer.send(channel, ...args);
+  },
+  on: (channel: string, listener: (...args: any[]) => void) => {
+    ipcRenderer.on(channel, (_, ...args) => listener(...args));
+  },
+  off: (channel: string, listener?: (...args: any[]) => void) => {
+    if (listener) {
+      ipcRenderer.off(channel, listener);
+    } else {
+      ipcRenderer.removeAllListeners(channel);
+    }
+  },
+  request: (channel: string, ...args: any[]): Promise<any> => {
+    return ipcRenderer.invoke(channel, ...args);
   },
 });
