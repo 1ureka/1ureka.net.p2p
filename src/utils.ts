295a64892a2c9c47cb1e985fc0251f1718c6b15f
1ureka <summerheadquarters001@gmail.com>
2025-10-01 06:23:57 +0800

    新增 defer 函數以支持外部 Promise 的解析與拒絕

--- src/utils.ts
+++ src/utils.ts
@@ -13,6 +13,18 @@ export async function tryCatch<T>(promise: Promise<T>): Promise<Result<T>> {
   }
 }
 
+export function defer<T>() {
+  let resolve!: (value: T | PromiseLike<T>) => void;
+  let reject!: (reason?: any) => void;
+
+  const promise = new Promise<T>((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+  return { promise, resolve, reject };
+}
+
 export type ConnectionStatus = "connected" | "disconnected" | "connecting" | "failed";
 export type ConnectionLogLevel = "info" | "warn" | "error";
 export type ConnectionLogEntry = {
