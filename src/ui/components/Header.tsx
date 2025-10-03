e9b73b1a461a0635850d9a9df472e7a3d5604b50
1ureka <summerheadquarters001@gmail.com>
2025-10-03 15:16:54 +0800

    重構 Header 組件，移除不必要的按鈕，調整樣式以改善佈局

--- src/ui/components/Header.tsx
+++ src/ui/components/Header.tsx
@@ -1,7 +1,6 @@
 import DirectionsBoatRoundedIcon from "@mui/icons-material/DirectionsBoatRounded";
 import HelpRoundedIcon from "@mui/icons-material/HelpRounded";
-import CodeRoundedIcon from "@mui/icons-material/CodeRounded";
-import { Box, BoxProps, Button, Divider, Tab, Tabs, Typography } from "@mui/material";
+import { Box, BoxProps, Button, Tab, Tabs, Typography } from "@mui/material";
 import { centerTextSx } from "@/ui/theme";
 import { useTabs } from "@/ui/tabs";
 
@@ -27,9 +26,6 @@ const HeaderLinks = () => {
       <Button color="inherit" sx={{ textTransform: "none", textWrap: "nowrap" }} startIcon={<HelpRoundedIcon />}>
         help & support
       </Button>
-      <Button color="inherit" sx={{ textTransform: "none", textWrap: "nowrap" }} startIcon={<CodeRoundedIcon />}>
-        source code
-      </Button>
     </Box>
   );
 };
@@ -43,6 +39,7 @@ const HeaderTabs = () => {
       onChange={(_, v) => setTab(v)}
       slotProps={{ indicator: { children: <span className="MuiTabs-indicatorSpan" /> } }}
       sx={{
+        mt: 2.5,
         "& .MuiTabs-indicator": { display: "flex", justifyContent: "center", bgcolor: "transparent" },
         "& .MuiTabs-indicatorSpan": { maxWidth: 40, width: 1, bgcolor: "primary.main" },
         "& .MuiTab-root": { textTransform: "none", "&:not(.Mui-selected):hover": { color: "text.primary" } },
@@ -57,23 +54,32 @@ const HeaderTabs = () => {
 
 const headerSx: BoxProps["sx"] = {
   px: 4,
-  pt: 2,
-  bgcolor: "background.paper",
+  bgcolor: "background.header",
   position: "sticky",
   top: 0,
   zIndex: 1,
   minWidth: "fit-content",
+  borderBottom: (theme) => `2px solid ${theme.palette.divider}`,
+};
+
+const headerInnerSx: BoxProps["sx"] = {
+  display: "grid",
+  alignItems: "center",
+  gap: 3,
+  gridTemplateColumns: "minmax(min-content, 1fr) minmax(min-content, auto) minmax(min-content, 1fr)",
+  "& > div:nth-of-type(1)": { justifySelf: "start" },
+  "& > div:nth-of-type(2)": { justifySelf: "center" },
+  "& > div:nth-of-type(3)": { justifySelf: "end" },
 };
 
 const Header = () => {
   return (
     <Box sx={headerSx}>
-      <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", gap: 2 }}>
+      <Box sx={headerInnerSx}>
         <HeaderTitle />
+        <HeaderTabs />
         <HeaderLinks />
       </Box>
-      <Divider sx={{ my: 2, borderWidth: 1.5 }} />
-      <HeaderTabs />
     </Box>
   );
 };
