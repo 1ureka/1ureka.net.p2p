46a849f0d994f9bb0748360ddb6e1bcbd61e00c8
1ureka <summerheadquarters001@gmail.com>
2025-10-05 18:26:04 +0800

    重構 createHostAdapter 函式，新增資源釋放，並整理封包處理邏輯

--- src/adapter/adapter-host.ts
+++ src/adapter/adapter-host.ts
@@ -1,6 +1,6 @@
 import net from "net";
-import { ipcMain, type BrowserWindow } from "electron";
 import { IPCChannel } from "@/ipc";
+import type { BrowserWindow } from "electron";
 
 import { createReporter } from "@/adapter/report";
 import { PacketEvent } from "@/adapter/packet";
@@ -10,9 +10,8 @@ import { SocketPairMap, stringifySocketPair, type SocketPair } from "@/adapter/i
 /**
  * 建立 Host 端的 Adapter (連接到本地的 TCP 伺服器)
  */
-function createHostAdapter(win: BrowserWindow) {
-  const { reportLog, reportWarn, reportError, clearHistory, reportConnection } = createReporter("Host", win);
-  clearHistory();
+function createHostAdapter(win: BrowserWindow, reporter: ReturnType<typeof createReporter>) {
+  const { reportLog, reportWarn, reportError, reportConnection } = reporter;
 
   const chunker = createChunker();
   const reassembler = createReassembler();
@@ -93,29 +92,23 @@ function createHostAdapter(win: BrowserWindow) {
   };
 
   /**
-   * 處理來自 RTC 端的 DATA 封包，將資料寫入對應的 TCP socket
-   */
-  const handleDataFromRTC = (socketPair: SocketPair, data: Buffer) => {
-    const success = sockets.get(socketPair)?.write(data);
-    if (!success) {
-      return reportError({
-        message: `Socket ${stringifySocketPair(socketPair)} does not exist or is not writable, cannot process incoming packet.`,
-      });
-    }
-  };
-
-  /**
-   * 處理來自 RTC 端的 CLOSE 封包，將對應的 TCP socket 關閉
-   */
-  const handleCloseFromRTC = (socketPair: SocketPair) => {
-    sockets.get(socketPair)?.destroy();
-    reportLog({ message: `TCP socket closed by remote client for socket ${stringifySocketPair(socketPair)}` });
-  };
-
-  /**
    * 處理來自 RTC 端的封包，並根據封包內容進行相應的處理
    */
   const handlePacketFromRTC = (_: unknown, buffer: Buffer) => {
+    const handleDataFromRTC = (socketPair: SocketPair, data: Buffer) => {
+      const success = sockets.get(socketPair)?.write(data);
+      if (!success) {
+        return reportError({
+          message: `Socket ${stringifySocketPair(socketPair)} does not exist or is not writable, cannot process incoming packet.`,
+        });
+      }
+    };
+
+    const handleCloseFromRTC = (socketPair: SocketPair) => {
+      sockets.get(socketPair)?.destroy();
+      reportLog({ message: `TCP socket closed by remote client for socket ${stringifySocketPair(socketPair)}` });
+    };
+
     try {
       for (const { pair, event, data } of reassembler.processPacket(Buffer.from(buffer))) {
         if (event === PacketEvent.CONNECT) {
@@ -125,9 +118,8 @@ function createHostAdapter(win: BrowserWindow) {
           const promise = socketPromises.get(pair);
 
           if (!promise) {
-            reportError({
-              message: `Socket ${stringifySocketPair(pair)} does not exist, cannot process incoming DATA packet.`,
-            });
+            const pairStr = stringifySocketPair(pair);
+            reportError({ message: `Socket ${pairStr} does not exist, cannot process incoming DATA packet.` });
           } else {
             // 利用 then 會順序執行的特性，保持 TCP 資料順序
             promise.then(() => handleDataFromRTC(pair, data));
@@ -143,8 +135,32 @@ function createHostAdapter(win: BrowserWindow) {
     }
   };
 
-  ipcMain.removeAllListeners(IPCChannel.FromRTC);
-  ipcMain.on(IPCChannel.FromRTC, handlePacketFromRTC);
+  // ------------------------------------------------------------------------------
+
+  // TODO: 管理動態 rules
+  const handleCreateRule = (_: unknown, _rule: unknown) => {
+    reportLog({ message: "Create rule - Not implemented yet" });
+  };
+
+  const handleRemoveRule = (_: unknown, _ruleId: unknown) => {
+    reportLog({ message: "Remove rule - Not implemented yet" });
+  };
+
+  // ------------------------------------------------------------------------------
+
+  /**
+   * 關閉 Adapter，釋放所有資源
+   */
+  const handleClose = () => {
+    for (const socket of sockets.values()) {
+      socket.destroy();
+      socket.removeAllListeners("data");
+      socket.removeAllListeners("error");
+      socket.removeAllListeners("close");
+    }
+  };
+
+  return { handlePacketFromRTC, handleClose, handleCreateRule, handleRemoveRule };
 }
 
 export { createHostAdapter };
