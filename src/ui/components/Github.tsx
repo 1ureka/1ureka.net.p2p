d6d1c1e43188339f13efbd66bc4bf4f7889d5209
1ureka <summerheadquarters001@gmail.com>
2025-10-09 22:55:15 +0800

    新增 GithubSwitch 組件，包含自定義樣式與圖示，改善開關的使用體驗

--- src/ui/components/Github.tsx
+++ src/ui/components/Github.tsx
@@ -1,6 +1,9 @@
+import HorizontalRuleRoundedIcon from "@mui/icons-material/HorizontalRuleRounded";
+import RadioButtonUncheckedRoundedIcon from "@mui/icons-material/RadioButtonUncheckedRounded";
 import { styled } from "@mui/material/styles";
-import { Box, Button, TextField, Tooltip, tooltipClasses } from "@mui/material";
-import type { BoxProps, TooltipProps } from "@mui/material";
+import { Box, Button, TextField, Tooltip, tooltipClasses, Typography } from "@mui/material";
+import { FormControlLabel, Switch } from "@mui/material";
+import type { BoxProps, TooltipProps, FormControlLabelProps } from "@mui/material";
 import { centerTextSx, generateColorMix } from "@/ui/theme";
 
 const tooltipMargin = "5px";
@@ -103,4 +106,89 @@ const GithubIconButton = styled(GithubButton)(({ theme }) => ({
   padding: theme.spacing(0.3),
 }));
 
-export { GithubTooltip, GithubButton, GithubIconButton, GithubTextField };
+const GithubSwitchBase = styled(Switch)(({ theme }) => ({
+  width: 46,
+  height: 23,
+  padding: 0,
+  "& .MuiSwitch-switchBase": {
+    "&:hover": { backgroundColor: "transparent" },
+    width: 23,
+    height: 23,
+    padding: 2,
+    transform: "translateX(0px)",
+    "& + .MuiSwitch-track": {
+      borderRadius: 6,
+      backgroundColor: theme.palette.action.disabled,
+    },
+    "& > .MuiSwitch-thumb": {
+      borderRadius: 6,
+      width: "100%",
+      height: "100%",
+      boxShadow: "none",
+      backgroundColor: theme.palette.background.header,
+    },
+    "&.Mui-checked": {
+      transform: "translateX(23px)",
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.primary.dark,
+      },
+      "& > .MuiSwitch-thumb": {
+        backgroundColor: theme.palette.text.primary,
+      },
+    },
+  },
+}));
+
+const switchIconSx: BoxProps["sx"] = {
+  display: "block",
+  fontSize: "16px",
+  transition: "all 0.2s",
+};
+
+type GithubSwitchProps = Omit<FormControlLabelProps, "control" | "label" | "checked" | "onChange"> & {
+  checked: boolean;
+  onChange: () => void;
+};
+
+const GithubSwitch = ({ sx, disabled, checked, onChange, ...props }: GithubSwitchProps) => (
+  <Box sx={{ position: "relative", opacity: disabled ? 0.5 : 1 }}>
+    <FormControlLabel
+      {...props}
+      disabled={disabled}
+      sx={{ m: 0, ...sx }}
+      control={<GithubSwitchBase disableRipple checked={checked} onChange={onChange} />}
+      labelPlacement="start"
+      label={
+        <Typography variant="caption" sx={{ ...centerTextSx, mr: 1 }}>
+          {checked ? "On" : "Off"}
+        </Typography>
+      }
+    />
+    <Box
+      sx={{
+        position: "absolute",
+        inset: 0,
+        left: "auto",
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
+        pointerEvents: "none",
+        width: 46,
+        height: 23,
+      }}
+    >
+      <Box sx={{ display: "grid", placeItems: "center" }}>
+        <HorizontalRuleRoundedIcon
+          sx={{ ...switchIconSx, rotate: "90deg", scale: checked ? "1" : "0.5", opacity: checked ? 1 : 0 }}
+        />
+      </Box>
+      <Box sx={{ display: "grid", placeItems: "center" }}>
+        <RadioButtonUncheckedRoundedIcon
+          sx={{ ...switchIconSx, color: "text.secondary", scale: checked ? "0.5" : "0.8", opacity: checked ? 0 : 1 }}
+        />
+      </Box>
+    </Box>
+  </Box>
+);
+
+export { GithubTooltip, GithubButton, GithubIconButton, GithubTextField, GithubSwitch };
