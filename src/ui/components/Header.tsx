355c3192393aba1c4f5b099081df8af690c31a71
1ureka <summerheadquarters001@gmail.com>
2025-10-13 03:31:03 +0800

    fix: prevent hang when peer closes today and adapter cannot be closed

--- src/ui/components/Header.tsx
+++ src/ui/components/Header.tsx
@@ -21,7 +21,7 @@ const HeaderTitle = () => {
           variant="body2"
           sx={{ display: { xs: "none", md: "block" }, color: "text.secondary", textAlign: "right", ...centerTextSx }}
         >
-          v1.0.0-alpha.10
+          v1.0.0-alpha.11
         </Typography>
       </Box>
     </Box>
